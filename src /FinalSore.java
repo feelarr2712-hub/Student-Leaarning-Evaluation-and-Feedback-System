4a2114edd8ce0fd45dbb21755fa91af14c93485a
khadra23ali-prog <khadra23ali@gmail.com>
2026-03-24 21:23:37 +0800

    Update FinalSore.java

--- src /FinalSore.java
+++ src /FinalSore.java
@@ -1,3 +1,18 @@
-//Member 6
+public static String calculateGrade(double totalScore) {
+    // Member 6 logic: Applying conditional decision making 
+    String grade;
 
-public class FinalScore {}
+    if (totalScore >= 80) {
+        grade = "A (Excellent)";
+    } else if (totalScore >= 70) {
+        grade = "B (Good)";
+    } else if (totalScore >= 60) {
+        grade = "C (Satisfactory)";
+    } else if (totalScore >= 50) {
+        grade = "D (Pass)";
+    } else {
+        grade = "F (Fail - Needs Improvement)";
+    }
+
+    return grade;
+}
