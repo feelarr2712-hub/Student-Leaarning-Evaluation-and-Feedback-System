ec49bf79c9f631191817691770e11b3d3d6b09fb
waiyanaumg777-star <waiyanaumg777@gmail.com>
2026-03-25 16:30:44 +0530

    Update FeedbackGenerator.java

--- src /FeedbackGenerator.java
+++ src /FeedbackGenerator.java
@@ -1,3 +1,20 @@
 //Member 7
+public class Feedback {
 
-public class FeedbackGenerator {}
+    public static void giveFeedback(double finalMarks) {
+
+        if (finalMarks >= 85) {
+            System.out.println("Feedback: Excellent performance! Keep it up!");
+        } 
+        else if (finalMarks >= 70) {
+            System.out.println("Feedback: Good job! You can do even better.");
+        } 
+        else if (finalMarks >= 50) {
+            System.out.println("Feedback: Average performance. Need improvement.");
+        } 
+        else {
+            System.out.println("Feedback: Poor performance. Work harder!");
+        }
+    }
+}
+
