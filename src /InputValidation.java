277386a0c43bfecac4928cde4de4138c0c3e3d0d
feelarr2712-hub <feelarr2712@gmail.com>
2026-03-26 13:22:03 +0300

    Implement input validation in InputValidation class
    Added input validation for assignment and exam scores.

--- src /InputValidation.java
+++ src /InputValidation.java
@@ -1,3 +1,35 @@
 //Member 3
 
-public class InputValidation {}
+import java.util.Scanner;
+
+
+public class InputValidation {
+
+    public static void main(String[]args)
+    {
+        double assignment = 0;
+        double exam = 0;
+
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter assignment");
+        assignment = in.nextDouble();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter student exam");
+        exam = input.nextDouble();
+
+
+
+        if (assignment < 0 || assignment > 100)
+            System.out.println("Invalid,\nTry again");
+        else if (exam < 0 || exam > 100)
+            System.out.println("Invalid \nTry again");
+        else
+            System.out.println("Loading...");
+
+
+
+    }
+}
