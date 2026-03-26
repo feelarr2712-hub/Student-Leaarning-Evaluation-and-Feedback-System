54d5175de7a0be63630fcca41c2d1204394a3d01
feelarr2712-hub <feelarr2712@gmail.com>
2026-03-26 13:22:59 +0300

    Remove main method from Main.java

--- src /Main.java
+++ src /Main.java
@@ -1,10 +1,5 @@
 //Member 1
 public class Main {
-    public static void main(String[] args) {
-        double finalMarks = 80;  // example
-
-        Feedback.giveFeedback(finalMarks);
-
-    }
+   
 }
 
