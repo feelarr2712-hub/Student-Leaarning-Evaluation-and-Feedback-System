26c4d595b7afea1c1f549e8197f703059f850110
waiyanaumg777-star <waiyanaumg777@gmail.com>
2026-03-27 18:21:43 +0530

    Update Main.java

--- src /Main.java
+++ src /Main.java
@@ -1,5 +1,22 @@
-//Member 1
 public class Main {
-   
-}
 
+    public static void main(String[] args) {
+
+        double finalMarks = 80; // example value
+
+        // Feedback (Member 7 part)
+        if (finalMarks >= 85) {
+            System.out.println("Excellent performance!");
+        }
+        else if (finalMarks >= 70) {
+            System.out.println("Good job!");
+        }
+        else if (finalMarks >= 50) {
+            System.out.println("Fair performance.");
+        }
+        else {
+            System.out.println("Poor performance.");
+        }
+
+    }
+}
