d246a193d8778daff3f5fe07d03cec6fdf005451
feelarr2712-hub <feelarr2712@gmail.com>
2026-03-30 19:05:29 +0300

    Refactor InputValidation class and remove unused methods
    Removed unused methods and cleaned up the InputValidation class.

--- src /InputValidation.java
+++ src /InputValidation.java
@@ -1,23 +1,15 @@
-
 //Member 3
+public class InputValidation
+{
+    static boolean getValidmark(double Marks)
+    {
 
-public class InputValidation {
 
+        if(Marks >= 0 && Marks <= 100 )
+            return true;
+        else
+            return false;
 
-    static double Input(double marks) {
-
-        while (marks >= 0 && marks <= 100) {
-           System.out.print(marks);
-            marks++;
-        }
-        return marks;
-    }
-
-    static String Validate(double marks) {
-      
-        if (marks > 100 || marks < 0) 
-            System.out.println("Invalid , /n Try again") ;
-        return"";
     }
 
 }
