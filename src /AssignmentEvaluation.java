ed3be34cfe1503c715236927478b350db1e8e6d4
feelarr2712-hub <feelarr2712@gmail.com>
2026-03-31 11:45:26 +0300

    Update AssignmentEvaluation.java

--- src /AssignmentEvaluation.java
+++ src /AssignmentEvaluation.java
@@ -1,3 +1,14 @@
 //Member 4
 
-public class AssignmentEvaluation {} 
+public class AssignmentEvaluation {
+
+
+    static double getAssignment( double Assignment1 , double Assignment2, double Assignment3) {
+
+        double assignmentMarks = (Assignment1 + Assignment2 + Assignment3) / 3;
+
+
+        double assignmentScore = (assignmentMarks * 0.4);
+        return assignmentScore;
+    }
+    }
